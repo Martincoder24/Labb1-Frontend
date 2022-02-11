ef284f17436d3d1cdd20e183a4d14af93c8838ed
Martincoder24 <80486477+Martincoder24@users.noreply.github.com>
2022-02-11 16:12:32 +0100

    Fixat en pop-up för customer cart

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+updateTotal();
+
 function myFunction() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
@@ -5,4 +7,23 @@ function myFunction() {
   } else {
     x.style.display = "block";
   }
+}
+
+function shopCart() {
+  var x = document.getElementById("shopping-menu");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
+//Anropa denna varje gång man klickar på att lägga till en kurs eller tar bort en kurs
+function updateTotal() {
+  document.getElementById(
+    "total"
+  ).innerHTML = `Total: <div class="value-color">${getCartTotal()}kr</div>`;
+}
+//Placeholder
+function getCartTotal() {
+  return 10;
 }
