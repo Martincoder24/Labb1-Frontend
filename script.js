4549d89ed839faa9500d751b097ffa0c701f301a
Martincoder24 <80486477+Martincoder24@users.noreply.github.com>
2022-02-15 19:06:32 +0100

    Lagt till en js grej som lägger till mina kurser

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+//Skaffa en cart som man kan spara kurserna i
+const cart = [];
+const courses = [];
+
+class Course {
+  constructor(input) {
+    this.kursNummer = input.kursNummer;
+    this.kursTitel = input.kursTitel;
+    this.kursBeskrivning = input.kursBeskrivning;
+    this.kursLängd = input.kursLängd;
+    this.kursPris = input.kursPris;
+    this.kursBild = input.kursBild;
+  }
+}
 updateTotal();
 
 function myFunction() {
@@ -27,3 +41,49 @@ function updateTotal() {
 function getCartTotal() {
   return 10;
 }
+
+function addCourseToCart(number) {
+  if(!cart.includes(number)){
+      cart.push(number); 
+  }else{
+    window.alert("Du kan inte lägga in samma kurs mer än en gång")
+  }
+  
+}
+
+function removeCourseFromCart() {
+
+}
+
+fetch("./allCourses.json")
+.then(response => response.json())
+.then(data=>{
+  const courseContainer = document.getElementById("course-container");
+  for (let i = 0; i < data.length; i++) {
+    const course = new Course(data[i]);
+    const div1 = document.createElement("div");
+    div1.classList.add("course");
+    const img = document.createElement("img");
+    img.src= course.kursBild;
+    const div2 = document.createElement("div");
+    div2.classList.add("overlay");
+    const div3 = document.createElement("div");
+    div3.classList.add("image-Title");
+    div3.innerText = course.kursTitel;
+    const p = document.createElement("p");
+    p.classList.add("image_description");
+    p.innerText = course.kursBeskrivning;
+    const a = document.createElement("a");
+    a.classList.add("add-btn");
+    a.setAttribute("onclick", `addCourseToCart(${course.kursNummer})`);
+    a.innerText = "Köp";
+    div1.appendChild(img);
+    div1.appendChild(div2);
+    div2.appendChild(div3);
+    div2.appendChild(p);
+    div2.appendChild(a);
+    courseContainer.appendChild(div1);
+    courses.push(course);
+  }
+
+})
