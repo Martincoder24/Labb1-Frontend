bd576c0afffdb08937b2c4e60b77a5c0d4abfec6
Martincoder24 <80486477+Martincoder24@users.noreply.github.com>
2022-02-17 22:03:09 +0100

    Uppdaterat add new course modal

--- script.js
+++ script.js
@@ -18,36 +18,46 @@ class Course {
 fetch("./allCourses.json")
   .then((response) => response.json())
   .then((data) => {
-    const courseContainer = document.getElementById("course-container");
     for (let i = 0; i < data.length; i++) {
       const course = new Course(data[i]);
-      const div1 = document.createElement("div");
-      div1.classList.add("course");
-      const img = document.createElement("img");
-      img.src = course.kursBild;
-      const div2 = document.createElement("div");
-      div2.classList.add("overlay");
-      const div3 = document.createElement("div");
-      div3.classList.add("image-Title");
-      div3.innerText = course.kursTitel;
-      const p = document.createElement("p");
-      p.classList.add("image_description");
-      p.innerText = course.kursBeskrivning;
-      const a = document.createElement("a");
-      a.classList.add("add-btn");
-      a.setAttribute("onclick", `addCourseToCart(${course.kursNummer})`);
-      a.innerText = "Köp";
-      div1.appendChild(img);
-      div1.appendChild(div2);
-      div2.appendChild(div3);
-      div2.appendChild(p);
-      div2.appendChild(a);
-      courseContainer.appendChild(div1);
       courses.push(course);
     }
-  })
+    getCourses();
+  });
+  
+function getCourses() {
+  const courseContainer = document.getElementById("course-container");
+  courseContainer.innerHTML = "";
+  for (let i = 0; i < courses.length; i++) {
+    const course = courses[i];
+    const div1 = document.createElement("div");
+    div1.classList.add("course");
+    const img = document.createElement("img");
+    img.src = course.kursBild;
+    const div2 = document.createElement("div");
+    div2.classList.add("overlay");
+    const div3 = document.createElement("div");
+    div3.classList.add("image-Title");
+    div3.innerText = course.kursTitel;
+    const p = document.createElement("p");
+    p.classList.add("image_description");
+    p.innerText = course.kursBeskrivning;
+    const a = document.createElement("a");
+    a.classList.add("add-btn");
+    a.setAttribute("onclick", `addCourseToCart(${course.kursNummer})`);
+    a.innerText = "Köp";
+    div1.appendChild(img);
+    div1.appendChild(div2);
+    div2.appendChild(div3);
+    div2.appendChild(p);
+    div2.appendChild(a);
+    courseContainer.appendChild(div1);
+  }
+}
 
-function myFunction() {
+
+
+function openMenu() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
     x.style.display = "none";
@@ -151,4 +161,29 @@ function emptyCart() {
     p.style.display = "none";
   }
   
+}
+//Öppna,stänga samt resetta modalen.
+function openModal () {
+  document.querySelector("form").reset();
+  const modal = document.getElementById("modal-new");
+  if(modal.style.visibility === "visible"){
+    modal.style.visibility = "hidden";
+  } else {
+    modal.style.visibility = "visible";
+  }
+}
+
+function addCourse() {
+  let checkImage = !document.getElementById("course-image").value.length == 0
+    const newCourse = {
+    kursNummer: Number(document.getElementById("course-number").value),
+    kursTitel: document.getElementById("course-title").value,
+    kursBeskrivning: document.getElementById("course-description").value,
+    kursLängd: document.getElementById("course-length").value,
+    kursPris: Number(document.getElementById("course-price").value),
+    kursBild: checkImage ? document.getElementById("course-image").value : "./Images/Defaultbild.png"
+  };
+  courses.push(new Course (newCourse));
+  getCourses();
+  openModal();
 }
